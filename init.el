9ce385448a7e4538200d72f752e8548e5058b48f
Suvrat Apte <suvrat@helpshift.com>
2018-01-19 12:12:22 +0530

    Stop loading 'setup-js.el' as it is deleted

--- init.el
+++ init.el
@@ -154,7 +154,7 @@
 
 ;; Langauage-specific
 (load "setup-clojure.el")
-(load "setup-js.el")
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
