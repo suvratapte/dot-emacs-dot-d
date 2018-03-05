484dc5442507a4d9ab0772c3fbe7a3cc2fa0fbf8
Suvrat Apte <suvrat@helpshift.com>
2018-03-05 16:37:00 +0530

    Move code from ui.el to init.el
    Also, delete ui.el.

--- init.el
+++ init.el
@@ -44,9 +44,54 @@
 ;; below, Emacs knows where to look for the corresponding file.
 (add-to-list 'load-path "~/.emacs.d/customizations")
 
-;; These customizations change the way emacs looks and disable/enable
-;; some user interface elements
-(load "ui.el")
+;; Turn off unecessary UI elements
+(menu-bar-mode -1)
+
+(when (fboundp 'tool-bar-mode)
+  (tool-bar-mode -1))
+
+(when (fboundp 'scroll-bar-mode)
+  (scroll-bar-mode -1))
+
+;; Show line numbers everywhere
+(global-linum-mode t)
+
+;; Custom themes path configuration
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+(add-to-list 'load-path "~/.emacs.d/themes")
+
+;; Set font size
+(set-face-attribute 'default nil :height 130)
+
+;; Use the 'Source Code Pro' font if available
+(when (member "Source Code Pro" (font-family-list))
+  (set-frame-font "Source Code Pro"))
+
+;; These settings relate to how emacs interacts with your operating system
+(setq ;; makes killing/yanking interact with the clipboard
+ x-select-enable-clipboard t
+
+ ;; I'm actually not sure what this does but it's recommended?
+ x-select-enable-primary t
+
+ ;; Save clipboard strings into kill ring before replacing them.
+ ;; When one selects something in another program to paste it into Emacs,
+ ;; but kills something in Emacs before actually pasting it,
+ ;; this selection is gone unless this variable is non-nil
+ save-interprogram-paste-before-kill t
+
+ ;; Shows all options when running apropos. For more info,
+ ;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Apropos.html
+ apropos-do-all t
+
+ ;; Mouse yank commands yank at point instead of at click.
+ mouse-yank-at-point t)
+
+;; No cursor blinking
+(blink-cursor-mode 0)
+
+;; Full path in the title bar
+(setq-default frame-title-format "Emacs (%f)")
 
 ;; These customizations make editing a bit nicer.
 (load "editing.el")
@@ -274,6 +319,10 @@
   :doc "Emacs Start Up Profiler (esup) benchmarks Emacs
         startup time without leaving Emacs."
   :ensure t)
+
+(setq org-list-demote-modify-bullet
+      '(("+" . "-") ("-" . "+") ("*" . "+")))
+(setq org-src-fontify-natively t)
 
 (if (eq system-type 'darwin)
     (use-package exec-path-from-shell
