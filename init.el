a124626745cdf50db2278b5fa1963fa41ecfe09f
Suvrat Apte <suvrat@helpshift.com>
2018-04-12 14:58:59 +0530

    Change the font color for doc strings

--- init.el
+++ init.el
@@ -172,7 +172,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(font-lock-doc-face ((t (:foreground "tan3")))))
 
 
 ;; Package configuration with 'use-package'
